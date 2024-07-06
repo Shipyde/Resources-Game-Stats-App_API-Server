61c3ac9312bb59406b631914302b00876f3e72b3
Shipyde <raecke@shipy.de>
2024-07-06 22:43:37 +0200

    Implementation of market data database retrieval + output of market data

--- api/v1/resources-game/routerResourcesGame.ts
+++ api/v1/resources-game/routerResourcesGame.ts
@@ -1,50 +1,81 @@
 import express, { Router, Request, Response } from "express";
+import {
+  mongooseClient,
+  mongooseClientDisconnect,
+} from "@/lib/database/mongooseClient";
 
 const routerResourcesGame: Router = express.Router();
 
 // RUFT DIE AKTION AUF DAMIT EINE EMAIL MIT EINEN EINMAL TOKEN GESENDET WIRD
-routerResourcesGame.get('/recovery/:UUID', function(req: Request, res: Response){
+routerResourcesGame.get(
+  "/recovery/:UUID",
+  function (req: Request, res: Response) {
     res.json({
-        msg: 'ok',
-        UUID: req.params.UUID
+      msg: "ok",
+      UUID: req.params.UUID,
     });
-});
+  }
+);
 
-// RUF DIE AKTION AUF DAMIT EINE EMAIL MIT EINEM EINMAL TOKEN ZUM 
+// RUF DIE AKTION AUF DAMIT EINE EMAIL MIT EINEM EINMAL TOKEN ZUM
 // LÖSCHEN DER HINTERLEGTEN DATENSÄTZE GESENDET WIRD
-routerResourcesGame.delete('/recovery/:UUID/', function(req:Request, res:Response){
+routerResourcesGame.delete(
+  "/recovery/:UUID/",
+  function (req: Request, res: Response) {
     res.json({
-        msg: 'Please continue the deletion process in the email sent to you.'
-    })
-})
-
-// GIBT EINMALIG DIE ZUR UUID IN DER DATENBANK GESPEICHERTEN DATEN ZURÜCK 
-// ODER BESTÄTIGT DAS LÖSCHEN EINES GESPEICHERTEN DATENSATZES
-routerResourcesGame.get('/recovery/:UUID/:TOKEN', function(req:Request, res:Response){
-    res.json({
-        msg: 'ok',
-        UUID: req.params.UUID,
-        TOKEN: req.params.TOKEN
+      msg: "Please continue the deletion process in the email sent to you.",
     });
-});
+  }
+);
+
+// GIBT EINMALIG DIE ZUR UUID IN DER DATENBANK GESPEICHERTEN DATEN ZURÜCK
+// ODER BESTÄTIGT DAS LÖSCHEN EINES GESPEICHERTEN DATENSATZES
+routerResourcesGame.get(
+  "/recovery/:UUID/:TOKEN",
+  function (req: Request, res: Response) {
+    res.json({
+      msg: "ok",
+      UUID: req.params.UUID,
+      TOKEN: req.params.TOKEN,
+    });
+  }
+);
 
 // SPEICHERT DIE DATEN AUS DER APP AB
-routerResourcesGame.post('/recovery', function(req:Request, res:Response){
-    res.json({
-        msg: 'ok'
-    });
+routerResourcesGame.post("/recovery", function (req: Request, res: Response) {
+  res.json({
+    msg: "ok",
+  });
 });
 
-
 // HOLT MARKTDATEN AUS DATENBANK
-routerResourcesGame.get('/data', function(req:Request, res:Response){
+routerResourcesGame.get("/data", async function (req: Request, res: Response) {
+  const { MarktData } = await mongooseClient();
+
+  try {
+    const data = await MarktData.findOne()
+      .sort({ createdAt: -1 })
+      .then((feedback) => feedback);
+
+    const disconnect = await mongooseClientDisconnect();
+
+    if (!data) {
+      res.json({
+        msg: "No market data found",
+      });
+    } else {
+      res.json({
+        data: data,
+      });
+    }
+  } catch (error) {
     res.json({
-        msg: 'ok'
+      msg: "Error by getting market data",
     });
+  }
 });
 
 module.exports = routerResourcesGame;
-
 
 /*
 
