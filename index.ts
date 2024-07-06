bf2b45981124191ef12235b2deb50a7333a98431
Shipyde <raecke@shipy.de>
2024-07-07 00:51:59 +0200

    Change server startup message

--- index.ts
+++ index.ts
@@ -2,23 +2,22 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
-var path = require('path');
-var cookieParser = require('cookie-parser');
+var path = require("path");
+var cookieParser = require("cookie-parser");
 const app: Express = express();
 const port = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-const v1 = require('./api/v1/routerv1');
-app.use('/v1', v1);
+const v1 = require("./api/v1/routerv1");
+app.use("/v1", v1);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
 });
-
 
 // catch 404 and forward to error handler
 /*app.use(function(req, res, next) {
@@ -32,9 +31,8 @@ app.get('/', (req: Request, res: Response) => {
     res.redirect('https://shipy.de/');
   });*/
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`API-Server listening on port ${port}`);
 });
 
 module.exports = app;
